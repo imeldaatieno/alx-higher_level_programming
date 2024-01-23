60ced879f2a350263cc095f6a0d03363ef5edb3c
Imelda Atieno <imeldaatieno01@gmail.com>
2024-01-23 01:49:04 -0800

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     my_list = [1, 2, 3, 4, 5]
     x = 5
-    while True:
-        try:
-            for i in my_list:
-                print(i)
+     for i in range(x):
+         try:
+                print("{}".format(my_list[i], end="")
         except IndexError:
             print("Invalid")
